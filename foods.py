b40b775a1da8e857eff12093e5fc9382595d9888
Dynamic <93918144+DynamicXD@users.noreply.github.com>
2024-10-20 23:41:17 +0530

    Create foods.py

---
+++ foods.py
@@ -0,0 +1,18 @@
+food_list = [
+    {
+        "name": "Biryani",
+        "price": 150
+    },
+    {
+        "name": "Fried Rice",
+        "price": 120
+    },
+    {
+        "name": "Noodles",
+        "price": 100
+    },
+    {
+        "name": "Meals",
+        "price": 130
+    }
+]
